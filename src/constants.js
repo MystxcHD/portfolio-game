d79e6afee236d5518d2289dba4a8a13adb133455
Siva Sricharan Dasaka <siva.dasaka75@gmail.com>
2024-05-18 20:05:07 -0400

    first commit

---
+++ src/constants.js
@@ -0,0 +1,37 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    computer: `This is where all the research and coding gets done. 
+    Here is my <a href="https://github.com/MystxcHD" target="_blank">Github</a>!`,
+
+    books: `I love to read up on the latest tech news, especially the new and exciting
+    field of AI and ML!`,
+
+    bed: `I'm a night owl and produce my best projects at night, but sleep is one of my
+    top priorties.`,
+
+    projects: `I'm an avid learner of the latest technologies, so I've doing a lot of experimentation
+    regarding AI/ML. I'm a hackathon fanatic and have completed projects regarding data analysis, machine learning, 
+    database management, and so much more! I'm certified in Python development for data science by IBM 
+    (<a href="https://courses.edx.org/certificates/8336d5f1aa1648c0b8b98f2d06fa9a55" target="_blank">Check it out here</a>)
+    and have extensive programming experience in Javascript, Java, HTML, and CSS. You can find full details on my LinkedIn 
+    <a href="https://www.linkedin.com/in/siva-sricharan-dasaka/" target="_blank">here</a>.`,
+
+    introFact: `Hey wanna play a game? There are 3 campfires scattered across the map,
+    each containing details on some of the projects I've built. Up for the challenge?`,
+
+    fact1: `When I first learned to code in the summer following my freshman year of high school, I built a Discord bot
+    called Tabby which had a bunch of fun minigames like 8-ball, insults, and a dice simulator. Tabby came with
+    a fully functioning economy as well. During winter break of the same year, I created Coinfinder, another Discord bot, but
+    this one used API calls to fetch market data on cryptocurrencies and all the analytics regarding each coin. These projects were
+    essential as they taught me a ton about programming, algorithms, and how to efficiently read documentation.`,
+
+    fact2: `I've built a machine learning model that takes in the molecular descriptor of a compound
+    and predicts the solubility. This was a lot of fun to make and I learned a lot about various techniques 
+    such as tree regressors and model validation.`,
+
+    fact3: `During a 24 hours high school hackathon, our team built a website that centered around patients
+    who aren't able to seek medical attention but need it urgently. Our app connects patients to doctors online and 
+    uses artificial intelligence to give medical recommendations based off the patient's symptoms. We ended up winning the
+    hackathon and I got a brand new monitor :D`,
+}
